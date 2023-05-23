18eaff79c10955e5ba5c4c6cfee5c03b24584075
mhmalekian <malekian610@gmail.com>
2023-05-24 01:22:26 +0330

    fix bug

--- src/Mt4trader.php
+++ src/Mt4trader.php
@@ -6,8 +6,9 @@ class Mt4trader extends Mttrader
 {
     const __API_BASE_URL="https://mt4.mtapi.be/";
 
-    function __construct()
+    function __construct($user,$password,$host,$port)
     {
+        parent::__construct($user,$password,$host,$port);
         parent::SetBaseUrl( Mt4trader::__API_BASE_URL);
     }
     
--- src/Mt5trader.php
+++ src/Mt5trader.php
@@ -6,8 +6,9 @@ class Mt5trader extends Mttrader
 {
     const __API_BASE_URL="https://mt5.mtapi.be/";
 
-    function __construct()
+    function __construct($user,$password,$host,$port)
     {
+        parent::__construct($user,$password,$host,$port);
         parent::SetBaseUrl( Mt5trader::__API_BASE_URL);
     }
 
